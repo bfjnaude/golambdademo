6752e411303844c261721a003eabcf4c04353004
Francois Naude <bfjnaude@gmail.com>
2018-07-17 14:40:35 +0200

    initial commit

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"errors"
+	"log"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+// ErrNameNotProvided is thrown when no name is provided in an HTTP request body
+var ErrNameNotProvided = errors.New("Name not provided in http body")
+
+// Handler function for Lambda connected to APIGateway
+func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	log.Printf("Processing Lambda request %s\n", request.RequestContext.RequestID)
+
+	if len(request.Body) < 1 {
+		return events.APIGatewayProxyResponse{}, ErrNameNotProvided
+	}
+
+	return events.APIGatewayProxyResponse{
+		Body:       "Hello " + request.Body,
+		StatusCode: 200,
+	}, nil
+}
+
+func main() {
+	lambda.Start(Handler)
+}
---
+++ main_test.go
@@ -0,0 +1,36 @@
+package main_test
+
+import (
+	"testing"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/stretchr/testify/assert"
+	main "github.com/uafrica/golambdademo"
+)
+
+func TestHandler(t *testing.T) {
+	tests := []struct {
+		request events.APIGatewayProxyRequest
+		expect  string
+		err     error
+	}{
+		{
+			// Test that the handler responds ErrNameNotProvided when no name is given in the HTTP body
+			request: events.APIGatewayProxyRequest{Body: ""},
+			expect:  "",
+			err:     main.ErrNameNotProvided,
+		},
+		{
+			request: events.APIGatewayProxyRequest{Body: "Bob"},
+			expect:  "Hello Bob",
+			err:     nil,
+		},
+	}
+
+	for _, test := range tests {
+		response, err := main.Handler(test.request)
+		assert.IsType(t, test.err, err)
+		assert.Equal(t, test.expect, response.Body)
+	}
+
+}
